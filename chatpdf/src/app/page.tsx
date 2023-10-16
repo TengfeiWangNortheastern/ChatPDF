cb1508a5910b75c64195c4931caaac0cb05f64ea
TengfeiWang <wang.ten@northeastern.edu>
2023-10-16 00:46:38 -0400

    component db

---
+++ chatpdf/src/app/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-100 via-red-100 to-yellow-100 ">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join million of students, researcher and professional answer
+            questions and understand research with AI
+          </p >
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started
+                  <LogIn className="w-4 h-4 ml-2"></LogIn>
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ chatpdf/src/lib/db/index.ts
@@ -0,0 +1,2 @@
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
